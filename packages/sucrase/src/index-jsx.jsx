5a7bce6357212de27316ecfbc4d05ed52b2f23eb
Magnesium- <yyong007@outlook.com>
2022-09-04 23:35:34 +0800

    feat(init): init project

---
+++ packages/sucrase/src/index-jsx.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+const Jsx = () => {
+  return <div>Hello, Jsx!</div>
+}
+
+export default Jsx
---
+++ packages/sucrase/src/index-tsx.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+const Tsx: React.FC = () => {
+  return <div>Hello, Tsx!</div>
+}
+
+export default Tsx
